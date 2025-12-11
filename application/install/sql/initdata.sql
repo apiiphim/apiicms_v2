e64f9f6bff31142b6cec66a7ffa0f87761c7a49c
apiiphim <apii.phim@gmail.com>
2025-12-11 15:46:15 +0700

    Update initdata.sql

--- application/install/sql/initdata.sql
+++ application/install/sql/initdata.sql
@@ -4,7 +4,7 @@
 INSERT INTO `mac_type` VALUES
 (1, 'Phim Bộ', 'phim-bo', 0, 1, 0, 1, 'type.html', 'show.html', 'detail.html', '', '', '', '', '', '', '{"class":"","area":"","lang":"","year":"","star":"","director":"","state":"","version":""}', '', '', ''),
 (2, 'Phim Lẻ', 'phim-le', 0, 1, 0, 1, 'type.html', 'show.html', 'detail.html', '', '', '', '', '', '', '{"class":"","area":"","lang":"","year":"","star":"","director":"","state":"","version":""}', '', '', ''),
-(3, 'Hoạt Hình', 'hoat-hinh', 0, 1, 0, 1, 'type.html', 'show.html', 'detail.html', '', '', '', '', '', '', '{"class":"","area":"","lang":"","year":"","star":"","director":"","state":"","version":""}', '', '', '');
+(3, 'Hoạt Hình', 'hoat-hinh', 0, 1, 0, 1, 'type.html', 'show.html', 'detail.html', '', '', '', '', '', '', '{"class":"","area":"","lang":"","year":"","star":"","director":"","state":"","version":""}', '', '', ''),
 (4, 'TV-Shows', 'tv-shows', 0, 1, 0, 1, 'type.html', 'show.html', 'detail.html', '', '', '', '', '', '', '{"class":"","area":"","lang":"","year":"","star":"","director":"","state":"","version":""}', '', '', '');
 
 
