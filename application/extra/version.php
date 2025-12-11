8d547d4e0d4d25b15c7859ab3ffb81cc5436c6e5
apiiphim <apii.phim@gmail.com>
2025-12-11 16:09:27 +0700

    Create version.php

---
+++ application/extra/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+    'name' => 'Phimcms',
+    'copyright' => 'Phimcms',
+    'url' => '//github.com/apiicms_v2',
+    'code' => '0.1.0',
+    'license' => 'Open source version',
+
+);
+?>
