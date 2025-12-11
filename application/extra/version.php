f1170355bc65bca95adf6b38b0d5d29368314b01
apiiphim <apii.phim@gmail.com>
2025-12-11 16:05:18 +0700

    Update version.php

--- application/extra/version.php
+++ application/extra/version.php
@@ -1,8 +1,8 @@
 <?php
 return array (
-    'name' => 'Mk-Porn',
-    'copyright' => 'Mk-Porn',
-    'url' => '//github.com/mk-porn',
+    'name' => 'Phimcms',
+    'copyright' => 'Phimcms',
+    'url' => '//github.com/apiicms_v2',
     'code' => '0.1.0',
     'license' => 'Open source version',
 
